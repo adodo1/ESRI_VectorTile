389d6e86f3981fb616f64df2044a86db1e6f1c35
dodo <dodo@dodo.com>
2017-09-18 23:25:48 +0800

    提交修改

--- bundle_to_mvt.py
+++ bundle_to_mvt.py
@@ -26,6 +26,44 @@ import os, sys, requests, struct, zlib, gzip, StringIO
 #   ...
 
 
+class GZipClass:
+    def GzipCompress(self, raw_data):
+        # 压缩
+        buf = StringIO()
+        f = gzip.GzipFile(mode='wb', fileobj=buf)
+        try:
+            f.write(raw_data)
+        finally:
+            f.close()
+        return buf.getvalue()
+      
+    def GzipUncompress(self, c_data):
+        # 解压
+        buf = StringIO(c_data)
+        f = gzip.GzipFile(mode = 'rb', fileobj = buf)
+        try:
+            r_data = f.read()
+        finally:
+            f.close()
+        return r_data
+
+    def CompressFile(self, fn_in, fn_out):
+        # 压缩文件
+        f_in = open(fn_in, 'rb')
+        f_out = gzip.open(fn_out, 'wb')
+        f_out.writelines(f_in)
+        f_out.close()
+        f_in.close()
+      
+    def UncompressFile(self, fn_in, fn_out):
+        # 解压文件
+        f_in = gzip.open(fn_in, 'rb')
+        f_out = open(fn_out, 'wb')
+        file_content = f_in.read()
+        f_out.write(file_content)
+        f_out.close()
+        f_in.close()
+
 
 class BundleClass:
     # 瓦片文件类
@@ -55,7 +93,7 @@ class BundleClass:
         # 计算row行col列在索引文件bundlx的偏移量
         pass
 
-    def HexToInt(self, value):
+    def HexToInt5(self, value):
         # 字节转整形
         # 例如: 0xFF00000000
         # 反序: 0x00000000FF
@@ -66,6 +104,16 @@ class BundleClass:
         result = (ord(value[4]) & 0xFF) << 32 | \
                  (ord(value[3]) & 0xFF) << 24 | \
                  (ord(value[2]) & 0xFF) << 16 | \
+                 (ord(value[1]) & 0xFF) << 8 | \
+                 (ord(value[0]) & 0xFF)
+        return int(result)
+
+    def HexToInt3(self, value):
+        # 字节转整形
+        # 例如: 0xFF0000
+        # 反序: 0x0000FF
+        # 再转成整数: 255
+        result = (ord(value[2]) & 0xFF) << 16 | \
                  (ord(value[1]) & 0xFF) << 8 | \
                  (ord(value[0]) & 0xFF)
         return int(result)
@@ -110,9 +158,50 @@ class BundleClass:
 
     def GetLevelRowCol(self):
         # 解析文件名 例如: L12/R0680C0c80.bundle
+        # 得到整个bundle瓦片包的开始 等级/行号/列号
         fname = self._fname
-        fname = os.path.abspath(fname).replace('\\', '/')
-        
+        bundledir, bundlename = os.path.split(fname)
+        bundledir = os.path.split(bundledir)[1]
+        shotname, extension = os.path.splitext(bundlename)
+        # 获取等级
+        try:
+            level = int(bundledir[1:])
+        except ValueError as ex:
+            level = -1
+        # 获取行列
+        try:
+            row = int(shotname[1:5], 16)
+            col = int(shotname[6:10], 16)
+        except ValueError as ex:
+            row = 0
+            col = 0
+        #
+        return level, row, col
+
+    def ListTiles(self, startrow, startcol):
+        # 列出瓦片包里有效的瓦片
+        result = []
+        fbundle = open(self._fname, 'rb')
+        # 跳过头部64字节
+        fbundle.seek(64)
+        for col in range(startcol, startcol + 128):
+            for row in range(startrow, startrow + 128):
+                # 前5字节偏移量 后3个字节文件大小
+                values = fbundle.read(8)
+                offset = self.HexToInt5(values[0:5])    # 偏移量
+                tsize = self.HexToInt3(values[5:8])     # 瓦片大小
+                #
+                if (tsize <= 0): continue
+                tileinfo = {
+                    'row': row,
+                    'col': col,
+                    'offset': offset,
+                    'tsize': tsize
+                    }
+                result.append(tileinfo)
+        fbundle.close()
+        #
+        return result
 
     def ReadTile(self, level, row, col):
         # 读取瓦片数据
@@ -132,16 +221,19 @@ if __name__=='__main__':
     print 'Encode: %s' %  sys.getdefaultencoding()
 
     # 单元测试
+    bundle = BundleClass('data/L14/R1b80C3300.bundle')
 
     # 根据瓦片等级/X/Y获得所在瓦片包名
-    #bundle = BundleClass('')
     #print bundle.GetBundleName(14, 0x1b9b, 0x337b)
 
-    
+    # 获取文件名和目录名
+    level, row, col = bundle.GetLevelRowCol()
+    print level, row, col
 
-
-
-
+    # 列出有效瓦片
+    row, col = 7040, 13056
+    tilelst = bundle.ListTiles(row, col)
+    print len(tilelst)
 
 
 
